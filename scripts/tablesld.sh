0c74a037fd23913296b8df43515885cf10de0481
Sarven Capadisli <info@csarven.ca>
2013-06-12 13:33:36 +0200

    Minor

--- scripts/tablesld.sh
+++ scripts/tablesld.sh
@@ -3,5 +3,3 @@
 
 /usr/lib/tarql/target/appassembler/bin/tarql topics.q > "$funding"funding.topics.ttl
 /usr/lib/tarql/target/appassembler/bin/tarql calls.q > "$funding"funding.calls.ttl
-
-./tdbstats.sh
