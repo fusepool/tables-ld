1580aaf14c62e307fe586f31f54298bc48bb2f23
Sarven Capadisli <info@csarven.ca>
2013-06-12 10:48:55 +0200

    Minor

--- scripts/tablesld.tdbloader.sh
+++ scripts/tablesld.tdbloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./tablesld.config.sh
 
-java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/funding "$tablesld"import/funding.topics.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/funding "$tablesld"/import/funding.nt
 
 ./tdbstats.sh
