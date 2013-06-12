db5bbb510c5fc72878fa73c6e789aea8eb5a3fd6
Sarven Capadisli <info@csarven.ca>
2013-06-12 10:51:58 +0200

    Minor

--- scripts/tablesld.tdbloader.sh
+++ scripts/tablesld.tdbloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./tablesld.config.sh
 
-java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/funding "$tablesld"/import/funding.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/funding "$funding"/import/funding.nt
 
 ./tdbstats.sh
